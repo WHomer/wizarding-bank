ccc09847f415d2b3bdae4980981805fcf137c246
William Homer <billyhomeriii@gmail.com>
2019-03-20 13:27:08 -0600

    Created the Person class with initialize and response methods

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+
+  def initialize(name, money)
+    @name = name
+    @money = money
+  end
+
+  def response
+    "#{@name} has been created with #{@money} galleons in cash."
+  end
+
+end
